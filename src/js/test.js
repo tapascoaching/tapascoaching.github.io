8a0be76b2c8534fd76344bcd11608cd5c1f25a28
Vishal Chandola <vishalchandola90@gmail.com>
2017-12-29 00:35:39 +0530

    Events gallery
    Events gallery slider emplimentation done.

--- src/js/test.js
+++ src/js/test.js
@@ -1,29 +1,12 @@
-/*var obj = {
-  one: {
-    fname: 'Vishal',
-    lname: 'Chandola'
-  },
-  two: {
-    fname: 'Gulshan',
-    lname: 'kumar'
-  },
-  three: {
-    fname: 'Vishal',
-    lname: 'Chandola'
-  },
-  four: {
-    fname: 'Vishal',
-    lname: 'Chandola'
-  }
-}
-
-const test = ({ one }) => {
-  return `Hi <strong>${one.fname} ${one.lname}</strong> how are you doing?`;
-};
-
-$(function() {
-  $('header').html(test(obj))
-})
-
-
-*/
+$(document).ready(function() {
+  $("#owl-demo").owlCarousel({
+    items : 1,
+    nav : true,
+    dots : false,
+    autoPlay : true,
+    slideSpeed : 200,
+    navText: ["",""],
+    lazyLoad : true,
+    baseClass : "owl-carousel"
+  });
+});
